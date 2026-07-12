86ddc66cadc93a41f5bf747ba5c1c4d72b5ca92d
lifepilot-jared <jared.hendriks6+lifepilot@gmail.com>
2026-07-13 08:57:02 +0930

    Load professional motion and school polish

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { SuperZoosDash } from "./game/SuperZoosDash";
 import "./game/startScreenFix.css";
 import "./game/schoolAdventurePass.css";
+import "./game/professionalUpgrade.css";
 
 export default function App() {
   return <SuperZoosDash />;
