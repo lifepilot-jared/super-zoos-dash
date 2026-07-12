a24ea67c1cdd1c14afa454f36b0fba0c535cee1a
lifepilot-jared <jared.hendriks6+lifepilot@gmail.com>
2026-07-12 14:49:41 +0930

    Add asset slots for future images and sounds

---
+++ src/game/assets.ts
@@ -0,0 +1,29 @@
+export const characterAssetSlots = {
+  peterSide: "",
+  superPeterSide: "",
+  judyCheer: "",
+} as const;
+
+export const villainAssetSlots = {
+  thunderWinnie: "",
+  iceMoe: "",
+} as const;
+
+export const plannedAssetFileNames = {
+  characters: [
+    "peter-side.png",
+    "super-peter-side.png",
+    "super-judy-cheer.png",
+  ],
+  villains: [
+    "thunder-winnie.png",
+    "ice-moe.png",
+  ],
+  sounds: [
+    "peter-trumpet.mp3",
+    "peter-shield.mp3",
+    "judy-delight.mp3",
+    "villain-silly.mp3",
+    "star-gem.mp3",
+  ],
+};
