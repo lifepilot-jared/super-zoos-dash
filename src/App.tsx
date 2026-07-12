7f1afb6ff828dad91e62f84b7fdea0581fa94560
lifepilot-jared <jared.hendriks6+lifepilot@gmail.com>
2026-07-13 09:16:25 +0930

    Load audio and motion verification fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { SuperZoosDash } from "./game/SuperZoosDash";
 import "./game/startScreenFix.css";
 import "./game/schoolAdventurePass.css";
 import "./game/professionalUpgrade.css";
+import "./game/audioMotionVerification.css";
 
 export default function App() {
   return <SuperZoosDash />;
