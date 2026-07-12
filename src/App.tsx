19467b6cc15968df2a92ad6241f6f38d842bee6e
lifepilot-jared <jared.hendriks6+lifepilot@gmail.com>
2026-07-12 17:31:32 +0930

    Load school adventure CSS pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { SuperZoosDash } from "./game/SuperZoosDash";
 import "./game/startScreenFix.css";
+import "./game/schoolAdventurePass.css";
 
 export default function App() {
   return <SuperZoosDash />;
